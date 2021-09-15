40ef65c8059f17fd4b171e15aee63ba0824c6f82
MaciYelaska <maciyelaska@creighton.edu>
2021-09-15 14:29:51 -0500

    added withSal to petNames

--- forms/pets/pets.js
+++ forms/pets/pets.js
@@ -5,3 +5,11 @@ Darcy, Jack, Jerry, Marty, Vito, Vinny, Riley, Saddie, Marge, Sam
 */
 
 let petNames = ['Darcy','Jack','Jerry','Marty','Vito','Vinny','Riley','Saddie','Marge','Sam']
+
+/*
+Create a second new array named 'withSal' that holds the original array data with the string "Sal" added to the end of the new array. 
+Hint: in Javascript, what would happen if you set 'withSal' equal to 'petNames', and then added "Sal" to the 'withSal' array?
+*/
+
+let withSal = petNames.push('Sal')
+console.log(petNames)
