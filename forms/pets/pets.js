3a0eef3ca8bc1f2a33106b7037d7e97a73aeebef
MaciYelaska <maciyelaska@creighton.edu>
2021-09-15 14:26:41 -0500

    Added petNames

---
+++ forms/pets/pets.js
@@ -0,0 +1,7 @@
+/* On the 'pets' form, create an array named 'petNames' that contains the names below.
+Hint: remember to add quotes around each name to show it is a string.
+
+Darcy, Jack, Jerry, Marty, Vito, Vinny, Riley, Saddie, Marge, Sam
+*/
+
+let petNames = ['Darcy','Jack','Jerry','Marty','Vito','Vinny','Riley','Saddie','Marge','Sam']
